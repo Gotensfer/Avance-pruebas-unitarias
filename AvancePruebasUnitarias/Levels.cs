261159f0c1657565ae91fd0501f7f348d18eb4ed
Jose Daniel Montoya Molina <66889936+panchuel@users.noreply.github.com>
2022-03-13 14:09:50 -0500

    A

---
+++ AvancePruebasUnitarias/Levels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Levels
+{
+	Enemy enemy;
+	public Player player;
+	public Levels()
+	{
+
+	}
+
+	public void GenerateEnemy()
+    {
+		enemy = new Enemy();
+    }
+
+	public void GeneratePlayer()
+    {
+		player = new Player();
+    }
+}
--- AvancePruebasUnitarias/Tower.cs
+++ AvancePruebasUnitarias/Tower.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AvancePruebasUnitarias
 {
-	public class Tower
+	public class Tower : Levels
 	{
-		public Tower()
+		List<Levels> towerPlayer = new List<Levels>(), towerEnemy = new List<Levels>();
+		Random rnd = new Random();
+		int amountLevels, positionPlayer;
+
+		public Tower(int amount)
 		{
+			amountLevels = rnd.Next(1, amount);
+			for (int i = 0; i < amountLevels; i++)
+			{
+				towerPlayer.Add(null);
+			}
 		}
+
+		void GenerateTower()
+        {
+			if(amountLevels <= 0) throw new Exception("Out of range");
+        }
+
+		void AssignPlayer()
+        {
+			positionPlayer = rnd.Next(0, towerPlayer.Count);
+            for (int i = 0; i < towerPlayer.Count; i++)
+            {
+				if (i == positionPlayer) towerPlayer[i].GeneratePlayer();
+            }
+        }
+
+
 	}
 }
