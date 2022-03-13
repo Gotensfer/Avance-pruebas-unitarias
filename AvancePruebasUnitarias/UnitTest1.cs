1ce27bcdf53c4384b4f5c4c4e5455c75ada9efdd
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-03-13 13:04:02 -0500

    Creacion de los metodos de prueba para las pruebas unitaria

--- AvancePruebasUnitarias/UnitTest1.cs
+++ AvancePruebasUnitarias/UnitTest1.cs
@@ -6,7 +6,55 @@ namespace AvancePruebasUnitarias
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreatedTower_HasAtLeastOneLevel()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreatedPlayerCharacter_HasStartingValue_GreaterThanZero()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerCharacter_IsCurrentlyIn_ALevelOfPlayerTower()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerFacesEnemy_CharacterWithGreaterValueWins_AndPlayerLoseOnTie()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerFacesObstacle_InPlayerTower_PlayerAlwaysWins()
+        {
+
+        }
+
+        [TestMethod]
+        public void OnFightResolve_WinningPlayerValueGets_LoserPlayerValue()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerFacesEnemy_PlayerWins_PTowerIncreasesOneLevel_ETowerDecreasesOneLevel()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerFacesLastRemaniningEnemy_PlayerWins_ETowerLevelIsAddedToPTower_ETowerDisappears()
+        {
+
+        }
+
+        [TestMethod]
+        public void PlayerFacesEnemy_EnemyHasGreaterValueThanPlayer_PlayerLoses_PlayerLosesOneLife()
         {
 
         }
